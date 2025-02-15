054a7657472f383c6d76e1ca46e612e78d3569d2
black1991518 <154672499+black1991518@users.noreply.github.com>
2025-02-15 22:40:41 +0900

    Update PlayerLevelEffects.kt

--- src/second_in_command/skills/PlayerLevelEffects.kt
+++ src/second_in_command/skills/PlayerLevelEffects.kt
@@ -67,19 +67,19 @@ object PlayerLevelEffects {
         var playerLevel = player.stats.level
 
         if (playerLevel >= 10) {
-            data.commander.stats.officerNumber.modifyFlat("sc_level_up_effect", 2f)
+            data.commander.stats.officerNumber.modifyFlat("sc_level_up_effect", 5f)
         }
 
         if (playerLevel >= 12) {
-            data.commander.stats.dynamic.getMod(Stats.OFFICER_MAX_LEVEL_MOD).modifyFlat("sc_level_up_effect", 1f)
+            data.commander.stats.dynamic.getMod(Stats.OFFICER_MAX_LEVEL_MOD).modifyFlat("sc_level_up_effect", 4f)
         }
 
         if (playerLevel >= 14) {
-            data.commander.stats.dynamic.getMod(Stats.OFFICER_MAX_ELITE_SKILLS_MOD).modifyFlat("sc_level_up_effect", 1f)
+            data.commander.stats.dynamic.getMod(Stats.OFFICER_MAX_ELITE_SKILLS_MOD).modifyFlat("sc_level_up_effect", 4f)
         }
 
         if (playerLevel >= 15 && Global.getSettings().modManager.isModEnabled("nexerelin")) {
-            data.commander.stats.dynamic.getStat("nex_max_agents").modifyFlat("sc_level_up_effect", 1f);
+            data.commander.stats.dynamic.getStat("nex_max_agents").modifyFlat("sc_level_up_effect", 4f);
         }
 
     }
